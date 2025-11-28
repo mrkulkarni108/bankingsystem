c156d4b611c9d2e724e6987e65e8857713a3d16a
mrkulkarni108 <1012411215@despu.edu.in>
2025-11-29 00:20:39 +0530

    Update server to custom ip's

--- server/Server.java
+++ server/Server.java
@@ -9,28 +9,26 @@ public class Server {
     public static void main(String[] args) {
 
         try {
-            // Set public IP from NGROK (host)
-            System.setProperty("java.rmi.server.hostname", "0.tcp.in.ngrok.io");
+            // Set public IP from NGROK (host) IF USING IT FOR WAN
+            System.setProperty("java.rmi.server.hostname", "<YOUR IPV4 PUBLIC IP>");
 
-            // Create REGISTRY on fixed port (MUST MATCH NGROK PORT 1)
-            LocateRegistry.createRegistry(1099);
+            LocateRegistry.createRegistry(1099);                 //YOU CAN ALWAYS GO WITH YOUR OPTIONAL PORT
             System.out.println("RMI Registry started on port 1099");
-
-            // Export BankImpl on fixed port 5000 (MUST MATCH NGROK PORT 2)
+            
             BankImpl obj = new BankImpl();   // already exported automatically
-            Naming.rebind("rmi://0.tcp.in.ngrok.io:14725/Bank", obj);
+            Naming.rebind("rmi://<YOUR IPV4 PUBLIC IP>/Bank", obj);
 
-            BankInterface stub = (BankInterface) UnicastRemoteObject.exportObject(obj, 5000);
+                BankInterface stub = (BankInterface) UnicastRemoteObject.exportObject(obj, 5000);// YOU CAN ALWAYS GO WITH YOUR OPTIONAL PORT
 
             // Bind using NGROK's registry port
-            Naming.rebind("rmi://0.tcp.in.ngrok.io:14725/Bank", stub);
+            Naming.rebind("rmi://<YOUR IPV4 PUBLIC IP>/Bank", stub);
 
             System.out.println("==============================================");
             System.out.println("        BANK SERVER RUNNING ON PUBLIC WAN      ");
             System.out.println("==============================================");
-            System.out.println("Registry Port     : 1099 (tunneled as 14725)");
-            System.out.println("Object Port       : 5000 (tunneled separately)");
-            System.out.println("Public RMI URL    : rmi://0.tcp.in.ngrok.io:14725/Bank");
+            System.out.println("Registry Port     : 1099");                              //(tunneled as 14725)
+            // System.out.println("Object Port       : 5000 (tunneled separately)"); USE THIS ONLY WHEN USING NGROK OR HOSTING WORLD WIDE
+            System.out.println("Public RMI URL    : rmi://<YOUR IPV4 PUBLIC IP>/Bank");
             System.out.println("==============================================");
 
         } catch (Exception e) {
@@ -39,3 +37,4 @@ public class Server {
         }
     }
 }
+
